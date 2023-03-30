b7ee60b3cd3fe6f7165bd896164da5a9d3c2c691
Graham-Broughton <broughton.graham@gmail.com>
2023-03-30 10:03:23 +0000

    working on EDA, time series plots done

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dataclasses import dataclass
+
+base = os.path.dirname(os.path.abspath(__file__))
+data = os.path.join(base, 'data')
+train = os.path.join(data, 'train')
+test = os.path.join(data, 'test')
+unlabeled = os.path.join(data, 'unlabeled')
+
+@dataclass
+class CFG:
+    NCOLS: int = 20
+    NROWS: int = 100
+    BASE_PATH: str = base
+    DATA_PATH: str = data
+    TRAIN_PATH: str = train
+    TEST_PATH: str = test
+    UNLABELED_PATH: str = unlabeled
+    
